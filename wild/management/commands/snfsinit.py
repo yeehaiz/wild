1f2e3469544a1b6348b0af19b116b689bf717739
yxdong <yxdong.cc@gmail.com>
2015-04-29 00:36:54 +0800

    .gitinore

--- wild/management/commands/snfsinit.py
+++ wild/management/commands/snfsinit.py
@@ -3,11 +3,17 @@
 '''
 创建snfs目录
 '''
-
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
 from helpers import snfs
+
+import os
 
 class Command(BaseCommand):
     def handle(self, *args, **option):
+        if not os.path.exists(settings.SNFS_DIR):
+            os.mkdir(settings.SNFS_DIR)
+
         snfs.folder_init()
         print "success!"
